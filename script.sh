e1d4b58909257abc1b428cb8ad1bd9c750d5884e
dilshad18 <34809568+dilshad18@users.noreply.github.com>
2018-12-05 13:46:33 +0530

    Delete script.sh

--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl get pods -n kube-system
-kubectl -n kube-system create sa tiller
-kubectl create clusterrolebinding tiller-cluster-rule     --clusterrole=cluster-admin     --serviceaccount=kube-system:tiller
-helm init --skip-refresh --upgrade --service-account tiller
-
